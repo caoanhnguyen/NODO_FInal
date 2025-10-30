f41de68dbf3f8803387822a7f17fa198f0a25250
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2025-10-30 09:21:46 +0700

    Initial commit

---
+++ src/main/java/com/example/nodo_final/NodoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.nodo_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodoFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodoFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nodo_final/NodoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nodo_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodoFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
